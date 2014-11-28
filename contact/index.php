a83b9394f51284b6ec05f48c39c4a8b09206bbd4
Daniel Jung <ilgoojung@hotmail.com>
2014-11-28 17:48:48 -0500

    Added Contact Page

---
+++ contact/index.php
@@ -0,0 +1,30 @@
+<html lang="en">
+  <head>
+    <link href='http://fonts.googleapis.com/css?family=Anton|Changa+One|Rubik+One' rel='stylesheet' type='text/css'>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js"></script>
+    <title>Daniel Jung</title>
+    <meta charset="utf-8"/>
+    <link type="text/css" href="../css/djstyle.css" rel="stylesheet" type="tet/css"/>
+		<script type="text/javascript" src="../javascript/djs.js"></script>
+		<script type="text/javascript" src="../plug-ins/jquery.animateSprite.min.js"></script>
+		<script type="text/javascript" src="../plug-ins/blocksjs-0.5.12.min.js"></script>
+		<script type="text/javascript" src="../javascript/treasure.js"></script>
+    <?php include "../global_elements/global_scripts.php"; ?>
+  </head>
+  <body>
+		<?php include "../global_elements/analyticstracking.php"; ?>
+		<?php navigation_bar("pm"); ?>
+		<div id="content_container">
+			<div id="content_background">
+				<div id="content">
+					
+				</div>
+			</div>
+    </div>
+    <div id="footer">
+			<?php footer_links();?>
+      Copyright 2014 &#169 Ilgoo Jung
+    </div>
+  </body>
+</html>
--- global_elements/global_scripts.php
+++ global_elements/global_scripts.php
@@ -7,30 +7,38 @@ echo'
       <div id="navigation">
         <u1 class="navbar">
           <li class="link">
-		    <div class="link">
-    	      <div class="navpointer"></div>
-		      <a rel="/" class="navlink'; 
+						<div class="link">
+							<div class="navpointer"></div>
+							<a rel="/" class="navlink'; 
 if (strcmp($input, "home") == 0) { echo ' active'; }
 
 echo '">Home</a>
             </div>
 		  </li>
           <li class="link">
-		    <div class="link">
-    	      <div class="navpointer"></div>
-		      <a rel="/experience" class="navlink';
+						<div class="link">
+							<div class="navpointer"></div>
+							<a rel="/experience" class="navlink';
 if ($input == "experience") { echo ' active'; }
 echo '">EXP</a>
             </div>
 		  </li>
           <li class="link">
-		    <div class="link">
-    	      <div class="navpointer"></div>
-		      <a rel="/inventory" class="navlink';
+						<div class="link">
+							<div class="navpointer"></div>
+							<a rel="/inventory" class="navlink';
 if ($input == "inventory") { echo ' active'; }
 echo '">Inventory</a>
             </div>
 	      </li>
+          <li class="link">
+						<div class="link">
+							<div class="navpointer"></div>
+							<a rel="/contact" class="navlink';
+if ($input == "pm") { echo ' active'; }
+echo '">PM</a>
+            </div>
+					</li>
         </u1>
       </div>
     </div>';
